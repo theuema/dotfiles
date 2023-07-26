434fd39c5fa1a209f562c00c777a626732941ac7
Mario Theuermann <mario@theuermann.dev>
2023-07-26 17:51:46 +0200

    Add utils to copy script

--- copy.sh
+++ copy.sh
@@ -4,18 +4,23 @@ main (){
 # locals
 local localhome=$(echo "$HOME")
 local home_config_path="${localhome}/.config"
-local dotfiles_copy_path="home"
-local config_copy_path="${dotfiles_copy_path}/.config"
-local library_copy_path="${dotfiles_copy_path}/Library"
+local copy_dir="home"
+local config_copy_path="${copy_dir}/.config"
+local library_copy_path="${copy_dir}/Library"
 local application_support_copy_path="${library_copy_path}/ApplicationSupport"
 local vscode_user_app_support_copy_path="${application_support_copy_path}/Code/User"
+local home_utils_path="${localhome}/.zshutils"
+local utils_copy_path="${copy_dir}/.zshutils"
 
 # create folder if not exist
-if [[ ! -d "$dotfiles_copy_path" ]]; then
-    mkdir -p $dotfiles_copy_path
+if [[ ! -d "$copy_dir" ]]; then
+    mkdir -p $copy_dir
 fi
 if [[ ! -d "$library_copy_path" ]]; then
     mkdir -p $library_copy_path
+fi
+if [[ ! -d "$utils_copy_path" ]]; then
+    mkdir -p $utils_copy_path
 fi
 if [[ ! -d "$vscode_user_app_support_copy_path" ]]; then
     mkdir -p $vscode_user_app_support_copy_path
@@ -24,24 +29,35 @@ fi
 # select dotfiles, linux- and mac-specific dotfiles
 sel_home_dotfiles=(.zshrc .zshfunc.zsh .aliases.zsh .zprofile .bashrc .bash_profile .p10k.zsh .tmux.conf .vimrc)
 sel_config_folders=(nvim vim git alacritty) #select folders to copy from $HOME/.config
+sel_zshutils=(utils gitutils dockerutils cheatnvim cheatmux cheatfzf cheatbash cheatansible)
+sel_mac_zshutils=(kp cu ci bu bi)
 
 if [[ "$1" == "-c" ]]; then # copy dotfiles to repo
     for d in "${sel_home_dotfiles[@]}"
-        do rsync -ahP "${localhome}/$d" "${dotfiles_copy_path}/"
+        do rsync -ahP "${localhome}/$d" "${copy_dir}/"
     done
     for d in "${sel_config_folders[@]}"
         do rsync -ahP --exclude={'.git','.github','.gitignore','.DS_Store'} "${home_config_path}/$d" "${config_copy_path}/"
     done
     # copy linux specific dotfiles
     if [[ "$(uname -a | awk '{print $1}')" != "Darwin" ]]; then
-        rsync -ahP "${localhome}/.linux.zsh" "${dotfiles_copy_path}/" 
+        rsync -ahP "${localhome}/.linux.zsh" "${copy_dir}/" 
     fi
-    # copy macOS specific dotfiles
+    # copy utils
+    for u in "${sel_zshutils[@]}"
+        do rsync -ahP "${home_utils_path}/$u" "${utils_copy_path}/"
+    done
+    # copy macOS specific dotfiles & utils
     if [[ "$(uname -a | awk '{print $1}')" == "Darwin" ]]; then
-        rsync -ahP "${localhome}/.mac.zsh" "${dotfiles_copy_path}/"
+        # dotfiles
+        rsync -ahP "${localhome}/.mac.zsh" "${copy_dir}/"
         rsync -ahP "${localhome}/Library/Preferences/com.googlecode.iterm2.plist" "${library_copy_path}/Preferences/"
         rsync -s -ahP "${localhome}/Library/Application Support/Code/User/keybindings.json" "${vscode_user_app_support_copy_path}/"
         rsync -ahP "${localhome}/Library/Application Support/Code/User/settings.json" "${vscode_user_app_support_copy_path}/"
+        # utils
+        for u in "${sel_mac_zshutils[@]}"
+            do rsync -ahP "${home_utils_path}/$u" "${utils_copy_path}/"
+        done
     fi
 else
     echo -e "Copy dotfiles to this repository with '-c' param\n"
